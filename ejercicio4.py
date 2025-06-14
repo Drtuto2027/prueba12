13f1189d9f5f5a2b042bf43ea6b3b08363048dde
unknown <molinabertulfo26@gmail.com>
2025-06-13 21:38:45 -0500

    se realizo cambios  ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,6 +1,6 @@
 # pedir la edad delusuario
 edad=int(input("ingresa tu edad"))
 #verificar si es mayor de edad 
-es_mayor_de_edad = edad >18
+es_mayor_de_edad = edad >30
 #mostrar eñ resultado
 print(f"¿eres mayor de edad:{es_mayor_de_edad}")
